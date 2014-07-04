3f1a32ccbffb43e288a66525a028c2f53e074ead
facualle@gmail.com <facualle@gmail.com@9a76d8cc-44cd-92dd-1e6a-93ded2fc3a7c>
2014-07-04 19:54:31 +0000

    -Cambios

--- trunk/FLP/FLP/index.Master.designer.cs
+++ trunk/FLP/FLP/index.Master.designer.cs
@@ -31,6 +31,15 @@ namespace FLP {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Control MainScriptManager.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager MainScriptManager;
+        
+        /// <summary>
         /// Control ContentIndex.
         /// </summary>
         /// <remarks>
--- trunk/FLP/FLP/index.aspx.cs
+++ trunk/FLP/FLP/index.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Logica;
 
 namespace FLP
 {
@@ -13,5 +14,42 @@ namespace FLP
         {
 
         }
+
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(5000);
+            try
+            {
+                ocultarPaneles();
+                //Registro de usuario en bd
+                GestorUsuario gestorUsuario = new GestorUsuario();
+                string codigo = gestorUsuario.registrarUsuario(txtNombre.Value, txtEmail.Value, txtContrasenia.Value);
+
+                //parámetros para mandar mail
+                string ActivationUrl = string.Empty;
+                string mail = txtEmail.Value;
+                string cuerpo = string.Empty;
+                ActivationUrl = Server.HtmlEncode("http://localhost:12434/admin/activar.usuario.aspx?UserCode=" + codigo);
+
+                GestorMails gestorMail = new GestorMails();
+                gestorMail.mandarMailActivacion(mail, "Activación de Cuenta", ActivationUrl);
+
+                btnRegistrar.Enabled = false;
+                panExito.Visible = true;
+                litMensaje.Text = "<strong>Se registró exitosamente su usuario.</strong><br />Revise su casilla de correo para activar su cuenta";
+            }
+            catch (Exception ex)
+            {
+                panFracaso.Visible = true;
+                litError.Text = ex.Message;
+            }
+        }
+
+        private void ocultarPaneles()
+        {
+            panExito.Visible = false;
+            panFracaso.Visible = false;
+        }
+        
     }
 }
